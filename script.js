dd202d037ce4a50097d9f19aecda0de7a726a057
Anani oluwatobiloba <ananioluwatobiloba2000@gmail.com>
2019-05-21 10:18:37 +0100

    modified

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let arrayOfNumbers = [122 ,0 ,0 ,1222];
+let arrayOfNumbers = [1225 ,777 ,345 ,1675];
 const personalDetails = {
     firstName : 'vanessa',
     lastName : 'rogers',
@@ -12,28 +12,18 @@ console.log("function that takes in an array of numbers");
 let numbers = ()=>{
     return `${arrayOfNumbers[0]} ${arrayOfNumbers[3]}`;
 
-}
+};
 console.log(numbers());
 
 
-console.log("********************************")
-// let numbers = (num ,num ,num ,num) =>{
-//     if (typeof(num) === ('number')){
-//         return arrayOfNumbers[$]
-//     }
-// }
-// let firstElement = arrayOfNumbers.map((x)=>x.[0]);
-// let lastElement = arrayOfNumbers.map((x)=>x.[3]);
-// let Numbers = (num1 ,num2 , num3 , num4)=>{
-//     return `${firstElement} ${lastElement}`;
+console.log("********************************");
 
-// }
 console.log("******************************************");
 console.log("FUNCTION THAT ACCEPTS AN OBJECT AS A PARAMETER");
 let details = ()=>{
         return `your name is ${personalDetails.firstName} ${personalDetails.lastName} ,
-    you are a ${personalDetails.gender} , your age is ${personalDetails.age} and your current salary is $${personalDetails.salary}`;
-}
+                you are a ${personalDetails.gender} , your age is ${personalDetails.age} and your current salary is $${personalDetails.salary}`;
+};
 console.log(details());
 console.log(personalDetails);
 console.log("******************************************");
@@ -48,12 +38,7 @@ let player = (name , score)=>{
        return `please input your name and score`;
    }
 
-}
+};
 console.log(player('tobi' , 200));
 // console.log(player);
 console.log("********************************************");
-
-
-
-// console.log(arrayOfNumbers);
-// console.log(numbers(12 , 34 , 77 , 837));
